aaebe5503665c27f7cfa6fed5111ba998cc0e414
Aman Adatia <adatiaaman@gmail.com>
2023-11-06 22:49:04 +0530

    submit button

--- client/src/components/Survey.jsx
+++ client/src/components/Survey.jsx
@@ -245,7 +245,7 @@ const Survey = ({ videoList }) => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleNext}
         >
-          Next
+          {currentVideoIndex === 19 ? 'Submit' : 'Next'}
         </button>
       </div>)}
       
